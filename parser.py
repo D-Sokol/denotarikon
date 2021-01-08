163aac00cef8a88cceb09c85801ad415849eb2f5
Sokol Dmitrii <sokol.dmitry.f@gmail.com>
2021-01-08 20:28:20 +0300

    Add input data validation in parser.py.

--- parser.py
+++ parser.py
@@ -1,4 +1,18 @@
 import argparse
+from functools import wraps
+
+
+def bounded_number(nmin=None, nmax=None, ntype=int):
+    @wraps(ntype)
+    def parse(value):
+        value = ntype(value)
+        if nmin is not None and nmin > value:
+            raise argparse.ArgumentTypeError(f"Invalid number: should be at least {nmin}, got {value}")
+        if nmax is not None and nmax < value:
+            raise argparse.ArgumentTypeError(f"Invalid number: should be at most {nmax}, got {value}")
+        return value
+    return parse
+
 
 def get_args(args=None):
     parser = argparse.ArgumentParser(prog='denotarikon')
@@ -6,10 +20,10 @@ def get_args(args=None):
     parser.add_argument('initial', nargs='?', default='', help='start generation from this phrase')
 
     model_group = parser.add_argument_group(title='Model', description='Settings affecting the text generation process')
-    model_group.add_argument('-n', '--number', type=int, default=1, help='Number of candidates to generate', dest='repeats')
-    model_group.add_argument('--threshold', type=float, default=0.95, metavar='P', help='Probability threshold for nucleus sampling')
-    model_group.add_argument('--temperature', type=float, default=1.0, metavar='TEMP')
-    model_group.add_argument('--max-tokens', type=int, default=5, help='Maximum number of *tokens* allowed per one letter')
+    model_group.add_argument('-n', '--number', type=bounded_number(1), default=1, help='Number of candidates to generate', dest='repeats')
+    model_group.add_argument('--threshold', type=bounded_number(0., 1., ntype=float), default=0.95, metavar='P', help='Probability threshold for nucleus sampling')
+    model_group.add_argument('--temperature', type=bounded_number(0., ntype=float), default=1.0, metavar='TEMP')
+    model_group.add_argument('--max-tokens', type=bounded_number(0), default=5, help='Maximum number of *tokens* allowed per one letter')
 
     device_group = parser.add_argument_group(title='Devices', description='Settings affectnig the devices used for text generation')
     device_group.add_argument('--no-cuda', '--no-gpu', default=True, action='store_false', help='Do not use GPU', dest='use_cuda')
